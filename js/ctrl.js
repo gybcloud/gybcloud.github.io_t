78f86a21d039b1e55b0cd17e25ac71b288a14d71
gybcloud <277478869@qq.com>
2017-06-21 20:12:04 +0800

    Delete ctrl.js

--- js/ctrl.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-			$("input[type='button']").click(function(){
-				var url ="https://api.47ks.com/webcloud/?url="+$("input[type='text']").val();
-				$("iframe").attr("src",url);
-			});
-		});
